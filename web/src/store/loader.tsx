06351727e8dec279a6d6c92b5ec7a933ec65521d
Daniel Gormly <danielgormly@hey.com>
2024-08-21 20:55:30 +1000

    WIP validation & loading w @borde/list integration

---
+++ web/src/store/loader.tsx
@@ -0,0 +1,74 @@
+import { NodeComponentType, Node, GenericNode } from "@borde/list";
+import { v, compile } from "suretype";
+import type { TypeOf } from "suretype";
+import { createUniqueId } from "solid-js";
+
+const GenericItemSchema = v.object({
+  id: v.string(),
+  content: v.string(),
+});
+
+type GenericItemSchema = TypeOf<typeof GenericItemSchema> & GenericNode<any>;
+
+export class GenericItem extends Node {
+  id: string;
+  type = "generic";
+  allowChildren = true;
+  tsCreated?: Date;
+  tsCompleted?: Date | null;
+  sticker?: string;
+  content?: string;
+  component = GenericComponent;
+  static validate = compile(GenericItemSchema, { simple: true });
+  constructor(props: GenericItemSchema) {
+    super(props);
+    this.id = props.id || createUniqueId();
+    this.content = props.content;
+  }
+  serialise() {
+    return {
+      id: this.id,
+      content: this.content,
+    };
+  }
+  updateContent(newText: string) {
+    this.content = newText;
+    this.triggerUpdate();
+  }
+}
+
+export function loader(data: any) {
+  if (data.type === "generic") {
+    const validated = GenericItem.validate(data);
+    console.log(validated);
+    if (!validated) return false;
+    return false;
+    // return new GenericItem(validated);
+  }
+  return false;
+}
+
+export const GenericComponent: NodeComponentType = (props) => {
+  const node = props.node.accessor;
+  return (
+    <div
+      aria-selected={props.ariaSelected}
+      // class={styles["tree-item"]}
+      onMouseDown={(event) => {
+        props.onMouseDown(event);
+      }}
+      onTouchStart={(event) => {
+        props.onTouchStart(event);
+      }}
+      onDblClick={(event) => {
+        event.preventDefault();
+        props.select();
+        // props.node.updateContent("gogogoo");
+      }}
+      data-index={props.index}
+      ref={props.ref}
+    >
+      {node().id} - {node().content}
+    </div>
+  );
+};
--- web/src/store/main.ts
+++ web/src/store/main.ts
@@ -1,12 +1,10 @@
-import {
-    IDBPDatabase, openDB, deleteDB,
-} from 'idb';
-import { ItemModel } from './item';
-import { ContainerModel } from './container';
-import { genTestData, bordeItems, inboxItems } from './dummy-data';
-import { ContainerFL, DoneFL, FastList } from './fast-list';
-import { v, compile } from 'suretype';
-import { createUniqueId } from 'solid-js';
+import { IDBPDatabase, openDB, deleteDB } from "idb";
+import { ItemModel } from "./item";
+import { ContainerModel } from "./container";
+import { genTestData, bordeItems, inboxItems } from "./dummy-data";
+import { v, compile } from "suretype";
+import { createUniqueId } from "solid-js";
+import { ListDragContext } from "@borde/list";
 
 const schemaVersion = 1;
 
@@ -16,43 +14,52 @@ interface DBTypes {
 }
 
 export type BordeIDB = IDBPDatabase<DBTypes>;
-export const dbNotReadyMessage = 'DB not loaded, pre-load buffer not yet implemented';
+export const dbNotReadyMessage =
+  "DB not loaded, pre-load buffer not yet implemented";
 
 // TODO: Retrieve these from model
-const itemStoreName = 'item';
-const doneStoreName = 'done';
-const containerStoreName = 'container';
+const itemStoreName = "item";
+const doneStoreName = "done";
+const containerStoreName = "container";
 // Remote Config store per browser (but could do local storage)
 
 const workspaceCache = v.object({
   activeWorkspace: v.string(),
-  workspaces: v.array(v.object({
-    id: v.string().required(),
-    name: v.string().required(),
-  })),
+  workspaces: v.array(
+    v.object({
+      id: v.string().required(),
+      name: v.string().required(),
+    }),
+  ),
 });
 
 /**
  * Session & workspace store
  */
 export class SessionStore {
-  userId: string = 'anonymous';
-  map = new Map<string, AcmeWorkspaceStore>;
+  userId: string = "anonymous";
+  map = new Map<string, AcmeWorkspaceStore>();
   workspace = new AcmeWorkspaceStore();
   constructor() {
     window.session = this;
   }
-  get cacheKey() { return `user_${this.userId}:cache`; }
+  get cacheKey() {
+    return `user_${this.userId}:cache`;
+  }
   loadWorkspaceCache() {
     const raw = localStorage.getItem(this.cacheKey);
     let parsed;
     if (raw) {
-      try { parsed = JSON.parse(raw); } catch (err) {
-        console.log('Invalid object found in cache');
+      try {
+        parsed = JSON.parse(raw);
+      } catch (err) {
+        console.log("Invalid object found in cache");
       }
     }
     if (parsed && compile(workspaceCache, { simple: true })(parsed)) {
-      parsed.workspaces?.forEach((workspace) => this.map.set(workspace.id, new AcmeWorkspaceStore(workspace)));
+      parsed.workspaces?.forEach((workspace) =>
+        this.map.set(workspace.id, new AcmeWorkspaceStore(workspace)),
+      );
       if (parsed.activeWorkspace) {
         this.open(parsed.activeWorkspace);
       } else if (this.map.size > 1) {
@@ -67,28 +74,30 @@ export class SessionStore {
     }
   }
   serialise() {
-    localStorage.setItem(this.cacheKey, JSON.stringify({
-      activeWorkspace: this.workspace?.id,
-      workspaces: Array.from(this.map.values()),
-    }));
+    localStorage.setItem(
+      this.cacheKey,
+      JSON.stringify({
+        activeWorkspace: this.workspace?.id,
+        workspaces: Array.from(this.map.values()),
+      }),
+    );
   }
   open(workspaceId: string) {
+    // TODO: Separate open/new workspace
     const workspace = this.map.get(workspaceId);
     if (workspace) {
       this.workspace = workspace;
     } else {
       this.workspace = new AcmeWorkspaceStore({
         id: createUniqueId(),
-        name: 'Offline workspace',
+        name: "Offline workspace",
       });
       this.map.set(this.workspace.id, this.workspace);
     }
-    this.workspace.connect()
+    this.workspace.connect();
     this.serialise();
   }
-  remove() {
-
-  }
+  remove() {}
   clear() {
     this.map = new Map();
   }
@@ -102,13 +111,13 @@ export class AcmeWorkspaceStore {
   itemModel = new ItemModel();
   containerModel = new ContainerModel();
   id: string = createUniqueId();
-  name: string = 'Uninitialised';
+  name: string = "Uninitialised";
   localOnly: boolean = true;
-  openLists = new Map<string, FastList>();
-  get ref () {
+  openLists = new Map<string, ListDragContext>();
+  get ref() {
     return `idb://${this.id}@${schemaVersion}`;
   }
-  constructor(workspace?: { id: string, name: string }) {
+  constructor(workspace?: { id: string; name: string }) {
     if (workspace) {
       this.id = workspace.id;
       this.name = workspace.name;
@@ -128,7 +137,7 @@ export class AcmeWorkspaceStore {
         console.debug(`Running upgrade`);
         await self.itemModel.upgrade(db);
         await self.containerModel.upgrade(db);
-        console.log('Completed upgrade');
+        console.log("Completed upgrade");
         // const doneStore = db.createObjectStore(doneStoreName, {
         //     keyPath: 'id',
         // });
@@ -140,67 +149,65 @@ export class AcmeWorkspaceStore {
     this.db = db;
     this.containerModel.load();
     return db;
-  }
+  };
   /**
    * A dev only route to delete and refresh db
    */
   reset = async () => {
-    console.log('Resetting database');
+    console.log("Resetting database");
     await this.db?.close();
-    await deleteDB(this.id)
-      .catch((err) => console.log(err));
-    console.log('Deleted DB');
+    await deleteDB(this.id).catch((err) => console.log(err));
+    console.log("Deleted DB");
     this.openLists.clear();
     await this.connect();
-  }
+  };
   dummyData = async () => {
     const items = [
-      ...genTestData('bordelist', bordeItems),
-      ...genTestData('inbox', inboxItems),
+      ...genTestData("bordelist", bordeItems),
+      ...genTestData("inbox", inboxItems),
     ];
     await this.itemModel.insert(items);
     await this.containerModel.insert([
       {
-        id: 'inbox',
-        name: 'Inbox',
-        icon: 'inbox',
-        sortKey: 'a',
+        id: "inbox",
+        name: "Inbox",
+        icon: "inbox",
+        sortKey: "a",
       },
       {
-        id: 'borde',
-        name: 'Borde',
-        icon: 'cutting-board',
-        sortKey: 'b',
+        id: "borde",
+        name: "Borde",
+        icon: "cutting-board",
+        sortKey: "b",
       },
       {
-        id: 'work',
-        name: 'a really really long named list',
-        icon: 'notepads',
-        sortKey: 'c',
+        id: "work",
+        name: "a really really long named list",
+        icon: "notepads",
+        sortKey: "c",
       },
     ]);
-  }
+  };
   openFastList(view: BordeView): FastList {
     let identifier = null;
     let fastList = null;
-    if (view.type === 'container') {
-        identifier = `c#${view.containerId}`;
-        fastList = this.openLists.get(identifier);
-        if (!fastList) {
-            fastList = new ContainerFL(this, view.containerId);
-            this.openLists.set(identifier, fastList);
-        }
+    if (view.type === "container") {
+      identifier = `c#${view.containerId}`;
+      fastList = this.openLists.get(identifier);
+      if (!fastList) {
+        fastList = new ContainerFL(this, view.containerId);
+        this.openLists.set(identifier, fastList);
+      }
     }
-    if (view.type === 'done') {
-        identifier = 'done';
-        fastList = this.openLists.get(identifier);
-        if (!fastList) {
-            fastList = new DoneFL(this);
-            this.openLists.set(identifier, fastList);
-        }
+    if (view.type === "done") {
+      identifier = "done";
+      fastList = this.openLists.get(identifier);
+      if (!fastList) {
+        fastList = new DoneFL(this);
+        this.openLists.set(identifier, fastList);
+      }
     }
-    if (!fastList) throw new Error('Cannot determine list from view');
+    if (!fastList) throw new Error("Cannot determine list from view");
     return fastList;
-}
-
+  }
 }
