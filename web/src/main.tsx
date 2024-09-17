3ab5a05c3376da0e8f38afde9b7fd5b34f1863a3
Daniel Gormly <danielgormly@hey.com>
2024-09-17 16:25:01 +1000

    Added window.* methods for debugging

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,25 +1,31 @@
 /* @refresh reload */
-import { render } from 'solid-js/web';
-import { App } from './app';
-import './main.css';
-import { SessionStore } from './store/main';
-import { sessionContext } from './store/context';
+import { render } from "solid-js/web";
+import { App } from "./app";
+import "./main.css";
+import { SessionStore } from "./store/main";
+import { sessionContext } from "./store/context";
+import { viewState } from "./view/state";
 
 // Setup app state
 const sessionStore = new SessionStore();
 sessionStore.loadWorkspaceCache();
 
+// Attach debug helpers to window
+window.session = sessionStore;
+window.viewState = viewState;
+
 // TODO: Render while store is alive (i.e. Allow models to run without db layer)
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 
 if (!(root instanceof HTMLElement)) {
-  throw new Error('App root missing.');
+  throw new Error("App root missing.");
 }
 
-render(() => (
-  <sessionContext.Provider value={sessionStore}>
-    {sessionStore.workspace && (
-      <App />
-    )}
-  </sessionContext.Provider>
-), root);
+render(
+  () => (
+    <sessionContext.Provider value={sessionStore}>
+      {sessionStore.workspace && <App />}
+    </sessionContext.Provider>
+  ),
+  root,
+);
