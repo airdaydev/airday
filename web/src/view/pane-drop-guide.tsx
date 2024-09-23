ac2633795394290fc9d1adc2474f65827c0c8886
Daniel Gormly <danielgormly@hey.com>
2024-09-23 17:41:37 +1000

    wip pane drop

--- web/src/view/pane-drop-guide.tsx
+++ web/src/view/pane-drop-guide.tsx
@@ -1,18 +1,34 @@
-import { createSignal } from "solid-js";
+import { createSignal, onMount } from "solid-js";
 import styles from "./view.module.css";
 
 export const PaneDropGuide = () => {
   const dynamicStyle = createSignal<string>("");
+  let dimensions = [0, 0];
+  let div: HTMLElement | undefined;
+  onMount(() => {
+    if (!div) return;
+    const bounds = div.getBoundingClientRect();
+    dimensions = [bounds.width, bounds.height];
+  });
   return (
     <div
-      onMouseOver={() => {
-        dynamicStyle[1]("background: purple;");
+      ref={div}
+      onMouseMove={(event: MouseEvent) => {
+        console.log(event.clientX, event.clientY);
+        // anchor right or left?
+        // anchor top or bottom?
+        // anchor full?
+        if (true)
+          dynamicStyle[1](
+            "position: absolute; top: 0; right: 0; width: 20px; height: 20px;",
+          );
       }}
       onMouseLeave={() => {
         dynamicStyle[1]("");
       }}
-      class={styles["pane-drop-guide"]}
-      style={dynamicStyle[0]()}
-    />
+      class={styles["pane-drop-guide-container"]}
+    >
+      <div class={styles["pane-drop-guide"]} style={dynamicStyle[0]()}></div>
+    </div>
   );
 };
