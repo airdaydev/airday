22b5119658254f37bc0f881a7baae0710ab53bc1
Daniel Gormly <danielgormly@hey.com>
2024-09-24 18:28:50 +1000

    Removed ai gl slop references

--- web/src/focus/focus.tsx
+++ web/src/focus/focus.tsx
@@ -1,7 +1,6 @@
 import styles from "./focus.module.css";
 import { GenericItem } from "../store/loader";
 import { viewState } from "../view/state";
-import { glSnow } from "./ai-gl";
 import { onMount } from "solid-js";
 
 interface FocusProps {
@@ -11,11 +10,8 @@ interface FocusProps {
 // Full screen view with Pomodoro timer
 // TODO: Change title of webpage to this
 export const Focus = (props: FocusProps) => {
-  let canvas: HTMLCanvasElement;
-  // onMount(() => glSnow(canvas));
   return (
     <div class={styles["container"]}>
-      <canvas ref={canvas} />
       <div class={styles["content"]}>
         <h1>{props.item.content}</h1>
         <button
