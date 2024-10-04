925ae3b33b9310b8cd446482e658ed5a53415d60
Daniel Gormly <danielgormly@hey.com>
2024-10-05 09:26:26 +1000

    Boilerplate for intermediate List solution

---
+++ web/src/store/list.ts
@@ -0,0 +1,37 @@
+import { ItemStore } from "./item-store";
+import { itemLoader } from "./item";
+import { SunlistWorkspace } from "./main";
+import { TreeState } from "@sunlist/list";
+import { Trx } from "./trx";
+
+export class List {
+  store: ItemStore;
+  workspace: SunlistWorkspace;
+  tree: TreeState;
+  constructor(store: ItemStore, workspace: SunlistWorkspace) {
+    this.store = store;
+    this.workspace = workspace;
+    this.tree = new TreeState({ loader: itemLoader(workspace) });
+    this.tree.context = this.workspace.listStateContext;
+    this.store.queue.subscribe(this.onTransaction.bind(this));
+  }
+  onTransaction(trx: Trx) {
+    if (trx.type === "check") {
+      const item = this.tree.idMap.get(trx.item.id);
+      // if (!item && trx.item.tsCompleted) {
+      //   this.tree.insertNode(
+      //     new GenericItem(trx.item, this.workspace),
+      //     null,
+      //     0,
+      //   );
+      // }
+      // if (item && !trx.item.tsCompleted) {
+      //   this.tree.delete(new Set([item]));
+      // }
+    }
+  }
+  async load() {
+    const itemsRaw = await this.store.loadCompletedItems();
+    this.tree.load({ children: itemsRaw });
+  }
+}
