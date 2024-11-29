d6e62998a8c56c8be53696e1525f86333d1fd314
Daniel Gormly <danielgormly@hey.com>
2024-11-29 12:47:48 +1100

    Removed original drag & drop

--- packages/list/src/index.tsx
+++ packages/list/src/index.tsx
@@ -3,5 +3,4 @@ export { TreeState, Node, ListStateContext } from "./state";
 export { type GenericNode, map, walk, filter } from "./tree-utils";
 export { type NodeComponentType } from "./node";
 export { DndContext, TreeContext, SolidListContext } from "./dnd-context";
-export { Dragged } from "./dragged";
 export { SoloNode } from "./solo-node";
