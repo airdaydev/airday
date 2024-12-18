d38259593b7ef62825dbb7596cf9f110257f748a
Daniel Gormly <danielgormly@hey.com>
2024-12-18 22:13:25 +1100

    Updates

--- packages/cal/src/state.ts
+++ packages/cal/src/state.ts
@@ -1,10 +1,9 @@
 import { CalendarEvent, CalendarEventConstructorProps } from "./event";
 
-export class EventState {
+export class EventDB {
   constructor() {}
   indexEvent(event: CalendarEvent) {
     // Place in Yearly/Monthly/Daily tree index
-    //
   }
   loadEvents(sEvents: CalendarEventConstructorProps[]) {
     for (let sEvent of sEvents) {
