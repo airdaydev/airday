c0d302d1c09a63c7a940519c57f73ea83ac128c4
Daniel Gormly <danielgormly@hey.com>
2024-12-19 08:33:58 +1100

    Updated calendar, cycling back brb

--- packages/cal/src/state.ts
+++ packages/cal/src/state.ts
@@ -1,14 +1,16 @@
 import { CalendarEvent, CalendarEventConstructorProps } from "./event";
 
 export class EventDB {
+  idMap = new Map<string, CalendarEvent>();
   constructor() {}
   indexEvent(event: CalendarEvent) {
-    // Place in Yearly/Monthly/Daily tree index
+    // Place in Yearly/Monthly tree index OR?
   }
   loadEvents(sEvents: CalendarEventConstructorProps[]) {
     for (let sEvent of sEvents) {
       const event = new CalendarEvent(sEvent);
       this.indexEvent(event);
+      this.idMap.set(event.id, event);
     }
   }
 }
