08630e40a044dabdc2aac9d0ed1e0b192051f81d
Daniel Gormly <danielgormly@hey.com>
2024-12-23 19:34:36 +1100

    Added a pre-existing interval-tree

--- packages/cal/src/state.ts
+++ packages/cal/src/state.ts
@@ -1,10 +1,13 @@
 import { CalendarEvent, CalendarEventConstructorProps } from "./event";
+import IntervalTree from "@flatten-js/interval-tree";
 
 export class EventDB {
   idMap = new Map<string, CalendarEvent>();
+  intervalTree = new IntervalTree();
   constructor() {}
   indexEvent(event: CalendarEvent) {
-    // Place in Yearly/Monthly tree index OR?
+    const range = {};
+    this.intervalTree.insert([1, 2], event);
   }
   loadEvents(sEvents: CalendarEventConstructorProps[]) {
     for (let sEvent of sEvents) {
