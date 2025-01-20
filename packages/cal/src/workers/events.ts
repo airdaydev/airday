e1944ddfd6725e5d2bfe68ddd058e62144f9d889
Daniel Gormly <danielgormly@hey.com>
2025-01-20 16:02:30 +1100

    Event position prep

--- packages/cal/src/workers/events.ts
+++ packages/cal/src/workers/events.ts
@@ -77,7 +77,7 @@ function scale() {
   canvas.height = transform.hourPx * 25 * transform.scale;
   ctx2D.scale(transform.scale, transform.scale);
   ctx2D.textBaseline = "top";
-  ctx2D.font = "8px Departure Mono";
+  ctx2D.font = "12px Departure Mono";
 }
 
 function getTime(dateNum: number) {
