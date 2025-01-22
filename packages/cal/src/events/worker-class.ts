72c72d1bc98621205affea91dd23d62f1a8f0c48
Daniel Gormly <danielgormly@hey.com>
2025-01-22 17:53:21 +1100

    wip worker refactor

--- packages/cal/src/events/worker-class.ts
+++ packages/cal/src/events/worker-class.ts
@@ -6,18 +6,13 @@ import {
   utcZeroDate,
 } from "../time";
 
-// This worker prepares events for rendering & renders them to an offscreen canvas
-// Range represents X clip dimension (i.e. visible days + buffer)
-let range: [number, number] = [0, 0];
-const idCache = new Map<string, any>();
-const cache = new Map<number, Set<any>>();
-const dirty = new Set<number>();
+interface Transform {
+  dayPx: number;
+  hourPx: number;
+  scale: number;
+}
 
-const transform = {
-  dayPx: 100,
-  hourPx: 25,
-  scale: 1,
-};
+// This worker prepares events for rendering & renders them to an offscreen canvas
 
 function addMapSet<K, V>(map: Map<K, Set<V>>, key: K, val: V) {
   const set = map.get(key);
@@ -29,38 +24,21 @@ function addMapSet<K, V>(map: Map<K, Set<V>>, key: K, val: V) {
   }
 }
 
-export function updateCache(
-  cache: Map<number, Set<any>>,
-  events: any[],
-  cacheRange: [number, number],
-) {
-  range = cacheRange;
-  events.forEach((event) => {
-    const start = Math.max(event.start, range[0] as number);
-    const end = Math.min(event.end, range[1] as number);
-    const startDay = localMidnight(new Date(start)).valueOf();
-    const endDay = localMidnight(new Date(end)).valueOf();
-    const days = Math.ceil((endDay - startDay) / 864e5) + 1;
-    for (let i = 0; i < days; i++) {
-      const day = addDaysNumber(startDay, 0); // utc start day
-      addMapSet(cache, day, event.id);
-      dirty.add(day);
-      idCache.set(event.id, event);
-    }
-  });
-}
-
-function renderCache(canvas, ctx2D) {
-  if (!ctx2D) throw new Error("offscreen ctx2d not ready");
+function renderCache(wrker: EventWorker) {
+  if (!wrker.ctx2D) throw new Error("offscreen ctx2d not ready");
   let j = 0;
   const map = new Map();
   // Cycle through each day
-  for (let clip = range[0]; clip <= range[1]; clip = addDaysNumber(clip, 1)) {
-    if (!dirty.has(clip)) {
+  for (
+    let clip = wrker.range[0];
+    clip <= wrker.range[1];
+    clip = addDaysNumber(clip, 1)
+  ) {
+    if (!wrker.dirty.has(clip)) {
       // Only rerender items marked as dirty
       continue;
     }
-    const events = cache.get(clip) || []; // Get events per day
+    const events = wrker.cache.get(clip) || []; // Get events per day
     const posMap = new Map<string, any>();
     // TODO: Check assumption that events have been sorted chronologically
     // Clusters [1,2,3...], Segments [1,2,3...]
@@ -98,11 +76,11 @@ function renderCache(canvas, ctx2D) {
       return clusterIndex;
     }
     events.forEach((id) => {
-      const event = idCache.get(id);
+      const event = wrker.idCache.get(id);
       const startTime = event.start < clip ? clip : event.start;
       const endTime = event.end > clip + 864e5 ? clip + 864e5 : event.end;
       const height = Math.max((endTime - startTime) / 1000 / 60, 22);
-      const y = timeToY(new Date(startTime), transform.hourPx);
+      const y = timeToY(new Date(startTime), wrker.transform.hourPx);
       const startsToday = event.start > clip;
       const segment = nextSegment(y, height);
       const cluster = nextCluster(y, height, segment);
@@ -117,17 +95,17 @@ function renderCache(canvas, ctx2D) {
       });
     });
     // console.log(new Date(clip), clusterSegments);
-    ctx2D.clearRect(0, 0, canvas.width, canvas.height);
+    wrker.ctx2D.clearRect(0, 0, wrker.canvas.width, wrker.canvas.height);
     let ops: (() => void)[][] = [];
     function addOp(segment: number, op: () => void) {
       if (!ops[segment]) ops[segment] = [op];
       else ops[segment].push(op);
     }
     events.forEach((id) => {
-      const event = idCache.get(id);
+      const event = wrker.idCache.get(id);
       const position = posMap.get(id);
       const cluster = clusterSegments[position.cluster];
-      const segmentSize = (transform.dayPx - 3) / cluster;
+      const segmentSize = (wrker.transform.dayPx - 3) / cluster;
       const x = segmentSize * position.segment;
       // Height calc
       // If event starts before today, event start is beginning of day
@@ -135,11 +113,11 @@ function renderCache(canvas, ctx2D) {
       // If event ends after today, event end time is end of day
       // If event ends today, event end time is end time
       addOp(position.segment, () => {
-        ctx2D.shadowColor = "#00000011";
-        ctx2D.shadowBlur = 3;
-        ctx2D.shadowOffsetX = 2;
-        ctx2D.shadowOffsetY = 2;
-        ctx2D.beginPath();
+        wrker.ctx2D.shadowColor = "#00000011";
+        wrker.ctx2D.shadowBlur = 3;
+        wrker.ctx2D.shadowOffsetX = 2;
+        wrker.ctx2D.shadowOffsetY = 2;
+        wrker.ctx2D.beginPath();
         const cornerRadii = [
           position.startsToday ? 2 : 0,
           position.startsToday ? 2 : 0,
@@ -147,57 +125,57 @@ function renderCache(canvas, ctx2D) {
           2,
         ];
         // outline
-        ctx2D.fillStyle = "black";
-        ctx2D.beginPath();
-        ctx2D.roundRect(
+        wrker.ctx2D.fillStyle = "black";
+        wrker.ctx2D.beginPath();
+        wrker.ctx2D.roundRect(
           segmentSize * position.segment - 0.5,
           position.y - 0.5,
-          transform.dayPx - x - 4,
+          wrker.transform.dayPx - x - 4,
           position.height + 1,
           cornerRadii,
         );
-        ctx2D.fill();
-        ctx2D.closePath();
-        ctx2D.beginPath();
-        // ctx2D.fillStyle = "rgb(255 240 190)"; // light
-        ctx2D.fillStyle = "rgb(64 60 48)";
-        ctx2D.roundRect(
+        wrker.ctx2D.fill();
+        wrker.ctx2D.closePath();
+        wrker.ctx2D.beginPath();
+        // wrker.ctx2D.fillStyle = "rgb(255 240 190)"; // light
+        wrker.ctx2D.fillStyle = "rgb(64 60 48)";
+        wrker.ctx2D.roundRect(
           segmentSize * position.segment,
           position.y,
-          transform.dayPx - x - 5,
+          wrker.transform.dayPx - x - 5,
           position.height,
           cornerRadii,
         );
-        ctx2D.fill();
-        ctx2D.closePath();
-        ctx2D.beginPath();
-        // ctx2D.fillStyle = "#ffdc68"; // light
-        ctx2D.fillStyle = "rgb(255 240 190/ 0.2)";
-        ctx2D.roundRect(x, position.y, 3, position.height, cornerRadii);
-        ctx2D.fill();
-        ctx2D.closePath();
-        ctx2D.shadowColor = "#00000000";
-        ctx2D.fillStyle = "rgb(152 136 102)";
+        wrker.ctx2D.fill();
+        wrker.ctx2D.closePath();
+        wrker.ctx2D.beginPath();
+        // wrker.ctx2D.fillStyle = "#ffdc68"; // light
+        wrker.ctx2D.fillStyle = "rgb(255 240 190/ 0.2)";
+        wrker.ctx2D.roundRect(x, position.y, 3, position.height, cornerRadii);
+        wrker.ctx2D.fill();
+        wrker.ctx2D.closePath();
+        wrker.ctx2D.shadowColor = "#00000000";
+        wrker.ctx2D.fillStyle = "rgb(152 136 102)";
         if (position.startsToday) {
           const path = new Path2D();
           path.rect(
             segmentSize * position.segment,
             position.y,
-            transform.dayPx - x - 5,
+            wrker.transform.dayPx - x - 5,
             position.height,
           );
-          ctx2D.save();
-          ctx2D.clip(path);
-          ctx2D?.fillText(`${event.title}`, x + 6, position.y + 4);
+          wrker.ctx2D.save();
+          wrker.ctx2D.clip(path);
+          wrker.ctx2D?.fillText(`${event.title}`, x + 6, position.y + 4);
           if (position.height > 24) {
-            ctx2D.fillStyle = "rgb(122 106 76)";
-            ctx2D?.fillText(
+            wrker.ctx2D.fillStyle = "rgb(122 106 76)";
+            wrker.ctx2D?.fillText(
               `${getTime(event.start)}`,
               x + 8,
               position.y + 4 + 16,
             );
           }
-          ctx2D.restore();
+          wrker.ctx2D.restore();
         }
       });
     });
@@ -205,11 +183,11 @@ function renderCache(canvas, ctx2D) {
       fmap.map((f) => f());
     });
     const utcDay = utcZeroDate(new Date(clip)).valueOf();
-    const bitmap = canvas.transferToImageBitmap();
+    const bitmap = wrker.canvas.transferToImageBitmap();
     map.set(utcDay, bitmap);
     j++;
-    // self.postMessage({ type: "day", date: utcDay, bitmap }, [bitmap] as any);
-    dirty.delete(clip);
+    // wrker.postMessage({ type: "day", date: utcDay, bitmap }, [bitmap] as any);
+    wrker.dirty.delete(clip);
   }
   map.forEach((v, k) =>
     self.postMessage({ type: "day", date: k, bitmap: v }, [v] as any),
@@ -219,6 +197,15 @@ function renderCache(canvas, ctx2D) {
 export class EventWorker {
   canvas: OffscreenCanvas;
   ctx2D: OffscreenCanvasRenderingContext2D;
+  transform: Transform = {
+    dayPx: 100,
+    hourPx: 25,
+    scale: 1,
+  };
+  range = [0, 0];
+  idCache = new Map<string, any>();
+  cache = new Map<number, Set<any>>();
+  dirty = new Set<number>();
   constructor() {
     self.addEventListener("message", this.onMessage);
     this.canvas = new OffscreenCanvas(100, 100);
@@ -228,28 +215,48 @@ export class EventWorker {
   }
   offscreenScale() {
     if (!this.ctx2D) throw new Error("offscreen ctx2d not ready");
-    this.canvas.width = transform.dayPx * transform.scale;
-    this.canvas.height = transform.hourPx * 25 * transform.scale;
-    this.ctx2D.scale(transform.scale, transform.scale);
+    this.canvas.width = this.transform.dayPx * this.transform.scale;
+    this.canvas.height = this.transform.hourPx * 25 * this.transform.scale;
+    this.ctx2D.scale(this.transform.scale, this.transform.scale);
     this.ctx2D.textBaseline = "top";
     this.ctx2D.font = "09px Departure Mono";
   }
   onMessage = (message: MessageEvent) => {
     if (message.data.type === "resize") {
       if (!this.ctx2D) throw new Error("offscreen ctx2d not ready");
-      transform.dayPx = message.data.params.dayPx || 100;
-      transform.hourPx = message.data.params.hourPx;
-      transform.scale = message.data.params.scale;
+      this.transform.dayPx = message.data.params.dayPx || 100;
+      this.transform.hourPx = message.data.params.hourPx;
+      this.transform.scale = message.data.params.scale;
       this.offscreenScale();
-      renderCache(this.canvas, this.ctx2D);
+      renderCache(this);
     }
     if (message.data.type === "load") {
       if (!this.ctx2D) throw new Error("offscreen ctx2d not ready");
-      updateCache(cache, message.data.events, message.data.range);
-      renderCache(this.canvas, this.ctx2D);
-      for (let i = new Date(range[0]).valueOf(); i < range[1]; i = i + 864e5) {
-        dirty.delete(i);
+      this.updateCache(message.data.events, message.data.range);
+      renderCache(this);
+      for (
+        let i = new Date(this.range[0]).valueOf();
+        i < this.range[1];
+        i = i + 864e5
+      ) {
+        this.dirty.delete(i);
       }
     }
   };
+  updateCache(events: any[], cacheRange: [number, number]) {
+    this.range = cacheRange;
+    events.forEach((event) => {
+      const start = Math.max(event.start, this.range[0] as number);
+      const end = Math.min(event.end, this.range[1] as number);
+      const startDay = localMidnight(new Date(start)).valueOf();
+      const endDay = localMidnight(new Date(end)).valueOf();
+      const days = Math.ceil((endDay - startDay) / 864e5) + 1;
+      for (let i = 0; i < days; i++) {
+        const day = addDaysNumber(startDay, 0); // utc start day
+        addMapSet(this.cache, day, event.id);
+        this.dirty.add(day);
+        this.idCache.set(event.id, event);
+      }
+    });
+  }
 }
