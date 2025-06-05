6fba42362f2872d6ddb83882846b0e5edc7fe3b1
Daniel Gormly <danielgormly@hey.com>
2025-06-05 20:57:09 +1000

    Extremely basic session auth

--- server/src/auth.rs
+++ server/src/auth.rs
@@ -1,6 +1,8 @@
 // TODO: This will initially handle session based authentication
 // via both a bearer token and cookie
-use axum::response::Json;
+use axum::{
+    extract::Request, http::StatusCode, middleware::Next, response::Json, response::Response,
+};
 use base64::{Engine as _, engine::general_purpose};
 use rand::{TryRngCore, rngs::OsRng};
 use serde::Serialize;
@@ -8,13 +10,13 @@ use tower_cookies::{Cookie, Cookies};
 
 fn gen_session_id() -> String {
     let mut rng = OsRng; // CSPRNG
-    let mut bytes = [0u8; 20]; // 160 bits of entropy
+    let mut bytes = [0u8; 20]; // 160 bits of entropy (OAuth 2 recommendation)
     rng.try_fill_bytes(&mut bytes).unwrap();
-    general_purpose::URL_SAFE_NO_PAD.encode(&bytes) // Base64 encode for URL-safe storage
+    general_purpose::URL_SAFE_NO_PAD.encode(&bytes) // Base64 encoded
 }
 
 #[derive(Serialize)]
-struct PwdAuthResponse {
+pub struct PwdAuthResponse {
     result: &'static str,
 }
 
@@ -32,10 +34,23 @@ pub async fn password_authorisation(cookies: Cookies) -> Json<PwdAuthResponse> {
     let cookie = Cookie::build(("session_id", session_id))
         .http_only(true)
         // .secure(true)
-        // .same_site(tower_cookies::cookie::SameSite::Strict)
+        .same_site(tower_cookies::cookie::SameSite::Strict)
         .path("/")
         .max_age(tower_cookies::cookie::time::Duration::hours(24))
         .build();
     cookies.add(cookie);
     Json(PwdAuthResponse::default())
+}
+
+pub async fn auth_middleware(
+    cookies: Cookies,
+    request: Request,
+    next: Next,
+) -> Result<Response, StatusCode> {
+    if let Some(session_cookie) = cookies.get("session_id") {
+        // TODO: Check session validity
+        Ok(next.run(request).await)
+    } else {
+        Err(StatusCode::UNAUTHORIZED)
+    }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -2,19 +2,28 @@ mod auth;
 mod config;
 mod jmap_core;
 mod server;
-use axum::{Router, routing::get};
+use axum::{Router, middleware, routing::get};
+use tower_cookies::CookieManagerLayer;
 
 #[tokio::main]
 async fn main() {
     let cfg = config::AirdayConfig::from_toml("port = 3000\nhost = '0.0.0.0'");
     let host_str = format!("{}:{}", cfg.host, cfg.port);
     println!("Airday server started at http://{}", host_str);
-    let app = Router::new()
+    let public = Router::new()
         .route("/", get(server::root_handler))
-        .route("/session", get(jmap_core::session_handler));
+        .route("/auth/pw", get(auth::password_authorisation));
+    let private = Router::new()
+        .route("/session", get(jmap_core::session_handler))
+        .layer(middleware::from_fn(auth::auth_middleware));
     let listener = tokio::net::TcpListener::bind(format!("{}", host_str))
         .await
         .unwrap();
+
+    let app = Router::new()
+        .merge(private)
+        .merge(public)
+        .layer(CookieManagerLayer::new());
 
     axum::serve(listener, app).await.unwrap();
 }
