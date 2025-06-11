1c7bb22d0d6c80670a77012273f63b67cbe09ebb
Daniel Gormly <danielgormly@hey.com>
2025-06-11 12:42:29 +1000

    Type guarded api calls

--- packages/client/client.spec.ts
+++ packages/client/client.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "bun:test";
 import { loadToml, validateConfig } from "toml-config";
-import { AirdayClient } from "./index";
+import { AirdayClient, getAPIRoot, createUser } from "./index";
 
 const schema = {
   API_URL: { type: "string" },
@@ -12,6 +12,14 @@ export const config = validateConfig(schema, rawConfig);
 const client = new AirdayClient(config.API_URL);
 
 test("getAPIRoot", async () => {
-  const d = await client.getAPIRoot();
-  expect(d).toHaveProperty("version");
+  const d = await getAPIRoot(client);
+  expect(d.body.version).toBeTypeOf("string");
+});
+
+test("createUser", async () => {
+  const d = await createUser(client, {
+    email: "daniel@gormly.co",
+    password: "fa09j20fiaj3fpaof",
+  });
+  console.log(d);
 });
--- packages/client/index.ts
+++ packages/client/index.ts
@@ -1,16 +1,52 @@
+import { compile, v, type EnsureFunction } from "suretype";
+
 export class AirdayClient {
   root = new URL("http://localhost:3000");
   constructor(rootURL: string) {
     this.root = new URL(rootURL);
   }
-  getAPIRoot() {
-    return fetch(this.root)
-      .then((response) => response.json())
-      .then((data) => {
-        return data;
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
+  endpoint(pathName: string) {
+    const url = new URL(this.root);
+    url.pathname = pathName;
+    return url;
   }
+  createUser() {}
+}
+
+interface AirdayJSONResponse<T> {
+  response: Response;
+  data: T;
+}
+
+type ExtractEnsureType<T extends EnsureFunction<any>> =
+  T extends EnsureFunction<infer U> ? U : never;
+
+async function validateJSONResponse<T extends EnsureFunction<any>>(
+  response: Response,
+  validator: T,
+): Promise<AirdayJSONResponse<ExtractEnsureType<T>>> {
+  let body = await response.json();
+  const data = validator(body);
+  return {
+    response,
+    data,
+  };
+}
+
+interface CreateUserOpts {
+  email: string;
+  password: string;
+}
+
+const createUserResponseSchema = v.object({
+  id: v.string(),
+  email: v.string(),
+});
+
+const validator = compile(createUserResponseSchema, { ensure: true });
+
+export function createUser(client: AirdayClient, opts: CreateUserOpts) {
+  return fetch(client.endpoint("/user"), {
+    method: "POST",
+  }).then((res) => validateJSONResponse(res, validator));
 }
