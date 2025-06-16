f271df8ead558e69f51fecf4b9ed495dc9afbfe9
Daniel Gormly <danielgormly@hey.com>
2025-06-16 16:12:25 +1000

    Updated prep script to work continuously

--- prep.sh
+++ prep.sh
@@ -4,7 +4,7 @@ set -e
 CONFIG_DIR=$HOME/.config/airday
 mkdir -p $CONFIG_DIR
 echo "export DATABASE_URL=sqlite:$CONFIG_DIR/airday.db" > .env
-sqlx database create
+sqlx database reset -y
 sqlx migrate run
 
 pnpm install
