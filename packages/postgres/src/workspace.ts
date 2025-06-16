63ebb3f0082f8a0aa7dcdd16267e3c2faa7286d2
Daniel Gormly <danielgormly@hey.com>
2025-06-16 13:45:42 +1000

    Removed unused postgres pkgs

--- packages/postgres/src/workspace.ts
+++
@@ -1,9 +0,0 @@
-import { DatabasePool, sql } from 'slonik';
-
-export const Workspace = (pool: DatabasePool) => {
-  const query = sql.unsafe`
-  CREATE TABLE workspace (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  );`;
-  return pool.query(query);
-}
