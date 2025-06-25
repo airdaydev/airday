9bef529a8a8d8e15558a96f0b9b090daff8095f6
Daniel Gormly <danielgormly@hey.com>
2025-06-25 14:19:58 +1000

    Table updates

--- migrations/000_dev.sql
+++ migrations/000_dev.sql
@@ -1,7 +1,61 @@
+-- notes on the data model
+-- 👻 ghost = deletion marker aka 🪦 tombstone
+-- ts = custom timestamp for lww-register ("{utc}/{pid}/{tick}")
+-- utc = native utc timestamp
+-- TODO: item.type could be an enum (repeat, static, series, shuffle, playlist)
+-- TODO: repeat could be a property...
+-- TODO: Consider an sql trigger for updating timestamps
+
 CREATE TABLE IF NOT EXISTS user (
   id UUID NOT NULL PRIMARY KEY,
   email TEXT NOT NULL UNIQUE,
   password_hash TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS item (
+  id UUID NOT NULL PRIMARY KEY,
+  text TEXT NOT NULL,
+  text_ts STRING NOT NULL,
+  type TEXT NOT NULL,
+  type_ts STRING NOT NULL,
+  repeat_break INTEGER NULL,
+  repeat_break_ts STRING NOT NULL,
+  repeat_target INTEGER NULL,
+  repeat_target_ts STRING NOT NULL,
+  updated_utc TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS item_ghost (
+  id UUID NOT NULL PRIMARY KEY,
+  container_id UUID NOT NULL,
+  item_id UUID NOT NULL,
+  rip_utc TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+
+CREATE TABLE IF NOT EXISTS container (
+  id UUID NOT NULL PRIMARY KEY,
+  title TEXT NOT NULL,
+  updated_utc TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS container_item (
+  id UUID NOT NULL PRIMARY KEY,
+  container_id UUID NOT NULL,
+  item_id UUID NOT NULL,
+  order_index INTEGER NOT NULL DEFAULT 0,
+  order_index_ts STRING NOT NULL,
+  FOREIGN KEY (container_id) REFERENCES container (id) ON DELETE CASCADE,
+  FOREIGN KEY (item_id) REFERENCES item (id) ON DELETE CASCADE,
+  UNIQUE(container_id, item_id)
+);
+
+CREATE TABLE IF NOT EXISTS container_item_ghost (
+  id UUID NOT NULL PRIMARY KEY,
+  container_id UUID NOT NULL,
+  item_id UUID NOT NULL,
+  order_index INTEGER NOT NULL,
+  order_index_ts STRING NOT NULL,
+  rip_utc TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );
 
 CREATE TABLE IF NOT EXISTS session (
