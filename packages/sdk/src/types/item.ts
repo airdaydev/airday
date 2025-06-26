55d53492f71bc3dc262d22b3bb96d71f5879af12
Daniel Gormly <danielgormly@hey.com>
2025-06-26 18:04:14 +1000

    Partial merge/update op

--- packages/sdk/src/types/item.ts
+++ packages/sdk/src/types/item.ts
@@ -1,4 +1,4 @@
-import type { LWWRegister } from "../client/lww";
+import type { LWW, LWWRegister } from "../client/lww";
 
 interface AirdayItemParams {
   id: string;
@@ -11,6 +11,16 @@ export class AirdayItem {
   constructor(params: AirdayItemParams) {
     this.id = params.id;
     this.text = params.text;
+  }
+  merge(fields: Partial<Omit<AirdayItemParams, "id">>) {
+    // TODO: If a server came back with a greater timestamp...
+    const updatePayloads = [];
+    if (fields.text) {
+      // TODO: We should test if the update succeeded and only add it to update object if so
+      this.text.merge(fields.text);
+      updatePayloads.push(["text", fields.text]); // TODO Something like this
+    }
+    return updatePayloads;
   }
   toJSON() {
     return {
