c7a1cd48002d62ae1babafdc016a4bf0928ca3fa
Daniel Gormly <danielgormly@hey.com>
2025-07-25 00:24:01 +1000

    added cal build to prep script

--- prep.sh
+++ prep.sh
@@ -7,5 +7,6 @@ echo "export DATABASE_URL=sqlite:$CONFIG_DIR/airday.db" > .env
 sqlx database reset -y --source sqlite/migrations
 envsubst < server/config_templates/config.toml > ./server/config.toml
 ./flatbuffers/compile.sh
+pnpm run --dir js/cal build
 
 pnpm install
