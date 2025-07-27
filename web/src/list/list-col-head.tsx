9fbed18cf1e306729abe0e5d79e80eed210b238a
Daniel Gormly <danielgormly@hey.com>
2025-07-27 13:26:41 +1000

    Cleaned up list header

--- web/src/list/list-col-head.tsx
+++ web/src/list/list-col-head.tsx
@@ -4,19 +4,14 @@ import styles from "./list.module.css";
 export const ListColumnHeaders = () => {
   return (
     <div class={styles["list-col-head"]}>
-      <span style={`width: 1rem;`} class={styles["list-col-head-item"]}>
-        <CheckSVG />
-      </span>
       <span
         class={styles["list-col-head-item"]}
         style="position: relative;
-        top: 1px;
-        width: 1em;
-        stroke-width: 2px;"
+        width: 1em;"
       >
-        Az
+        Item
       </span>
-      <span class={styles["list-col-head-item"]} style={`width: 4em;`}>
+      <span class={styles["list-col-head-item"]} style={`margin-left: auto;`}>
         Date
       </span>
     </div>
