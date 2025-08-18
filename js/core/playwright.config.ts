8363f5ffd19088e55aad71280a189d0415fbd706
Daniel Gormly <danielgormly@hey.com>
2025-08-18 11:08:41 +1000

    Removed unused lines

--- js/core/playwright.config.ts
+++ js/core/playwright.config.ts
@@ -1,13 +1,10 @@
 // playwright.config.ts
 import { defineConfig, devices } from "@playwright/test";
-import { fileURLToPath } from "url";
-import path from "path";
 
 const baseURL = "http://localhost:3002";
 
 export default defineConfig({
   use: {
-    // Use Firefox as default for all tests
     ...devices["Desktop Firefox"],
     baseURL,
   },
@@ -16,5 +13,4 @@ export default defineConfig({
     url: baseURL,
     reuseExistingServer: false,
   },
-  // ... other config
 });
