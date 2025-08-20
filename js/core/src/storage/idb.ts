e4c4d059f77559f92b895e8adf52e6ceb6cfd0d5
Daniel Gormly <danielgormly@hey.com>
2025-08-20 13:56:55 +1000

    Unification wip

--- js/core/src/storage/idb.ts
+++ js/core/src/storage/idb.ts
@@ -1,5 +1,5 @@
 import { type DBSchema, type IDBPDatabase, openDB, type StoreNames } from "idb";
-import { AirdayItem, SyncObject } from "../sync/model";
+import { SyncObject } from "../sync/model";
 
 const SYNC_STORE_NAME = "syncable";
 const LIBRARY_STORE_NAME = "library";
@@ -10,7 +10,6 @@ export interface AirdayDBSchema extends DBSchema {
     value: any;
     indexes: {
       libraryId: string;
-      // archived
     };
   };
   [LIBRARY_STORE_NAME]: {
@@ -26,17 +25,16 @@ export type AirdayStoreNames = StoreNames<AirdayDBSchema>;
 // Front-end persistent storage for Airday JS apps
 export class AirdayIDB {
   handle: AirdayIDBPDatabase | null = null;
-  item = new ItemIDBModel(this);
+  syncable = new SyncableIDB(this);
   constructor() {}
   connect = async () => {
     this.handle = await openDB("test", 1, {
       upgrade(db) {
-        const items = db.createObjectStore(SYNC_STORE_NAME, { keyPath: "id" });
-        items.createIndex("libraryId", "libraryId");
-        // items.createIndex("listId", "listId");
-        // items.createIndex("order", ["listId", "orderKey", "id"]);
-        // items.createIndex("done", ["doneTS"]); // TODO: Done timestamp?
-        const container = db.createObjectStore(LIBRARY_STORE_NAME, {
+        const objects = db.createObjectStore(SYNC_STORE_NAME, {
+          keyPath: "id",
+        });
+        objects.createIndex("libraryId", "libraryId");
+        const library = db.createObjectStore(LIBRARY_STORE_NAME, {
           keyPath: "id",
         });
       },
@@ -44,42 +42,42 @@ export class AirdayIDB {
   };
 }
 
-// TODO: Get archived items separately (Decide what should be archived and provide common index)
-export class ItemIDBModel {
+// TODO: Get archived objects separately (Decide what should be archived and provide common index)
+export class SyncableIDB {
   db: AirdayIDB;
   storeName = SYNC_STORE_NAME;
   constructor(db: AirdayIDB) {
     this.db = db;
   }
-  upsert = async (items: SyncObject[]) => {
+  upsert = async (objects: SyncObject[]) => {
     const tx = this.db.handle!.transaction(SYNC_STORE_NAME, "readwrite");
     const store = tx.objectStore(SYNC_STORE_NAME);
     const b = await store.getAll();
     // TODO: We also need to extract indexes in JSON version (e.g. done)!
     await Promise.all(
-      items.map((item) => {
-        return store.put(item.toJSON());
+      objects.map((obj) => {
+        return store.put(obj.toJSON());
       }),
     );
     await tx.done;
   };
-  getItemsByLibrary = async (libraryId: string) => {
+  getByLibrary = async (libraryId: string) => {
     const res = await this.db.handle!.getAllFromIndex(
       SYNC_STORE_NAME,
       "libraryId",
       libraryId,
     );
-    const items: SyncObject[] = [];
+    const objects: SyncObject[] = [];
     res.forEach((row) => {
       try {
-        items.push(SyncObject.fromJSON(row));
+        objects.push(SyncObject.fromJSON(row));
       } catch (err) {
         console.warn("Could not parse row from db", row, err);
       }
     });
-    return items;
+    return objects;
   };
-  deleteItems = async (hexIds: string[]) => {
+  delete = async (hexIds: string[]) => {
     // await this.db!.handle?.delete(SYNC_STORE_NAME, id);
     const tx = this.db.handle!.transaction(SYNC_STORE_NAME, "readwrite");
     const store = tx.objectStore(SYNC_STORE_NAME);
