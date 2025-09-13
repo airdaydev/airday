e549fbd57b12444ef7ca06685efe31622049dd34
Daniel Gormly <danielgormly@hey.com>
2025-09-13 18:52:08 +1000

    right direction

--- js/core/src/sync/item.ts
+++ js/core/src/sync/item.ts
@@ -1,5 +1,7 @@
+import { Accessor, createSignal, Signal } from "solid-js";
 import { LWWRegister } from "../crdt/lww";
 import { SyncObject, RegisterMap, KeyMap, Change } from "./sync-object";
+import { Uuidv4 } from "../common/uuid";
 
 export const ITEM = 0;
 
@@ -28,22 +30,48 @@ class ItemAttributesCodec {
   // runs validation in... both directions?
 }
 
+// TODO: Pull from core later
+// TODO: Could this be based off a consumer class?
+// TODO: Where to put setters?
 export class AirdayItem {
-  syncObject: SyncObject;
-  // TODO: create signal
+  private $text: Signal<string>;
+  private syncObject: SyncObject;
+  // TODO: create signal of all attributes (or... maybe just directly on AirdayItem)
   constructor(syncObject: SyncObject) {
     this.syncObject = syncObject;
     this.syncObject.subscribe(this.react);
-    // Initial population
+    this.$text = createSignal<string>("");
+    // Initial population of data?
+  }
+  get id() {
+    return this.syncObject.id;
+  }
+  get libraryId() {
+    return this.syncObject.libraryId;
+  }
+  get text() {
+    return this.$text[0];
+  }
+  // TODO: This may not be necessary with initial population
+  private internalValue(id: string) {
+    return this.syncObject.values[id]?.data;
   }
   react(change: Change) {
     // TODO: Map each change to update correct signal
     // i.e. decode & react
+    const n = Number(change[0]);
+    const d = change[1] as any; // TODO: Should we validate type?
+    switch (n) {
+      case ITEM_KEY_MAP.text: {
+        this.$text[1](d);
+        break;
+      }
+    }
   }
-  updateText(text: string) {
+  set text(val: string) {
     // TODO: The merge needs to be monotonic from the last seen timestamp, not just the last generated
     const newText = new LWWRegister({
-      data: text,
+      data: val,
     });
     // this.attributes.merge(0, newText);
   }
