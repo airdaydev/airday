9df86087484b36844218b479e8c5c27cc95f65f7
Daniel Gormly <danielgormly@hey.com>
2025-12-05 17:34:35 +1100

    Auth tests passing

--- js/core/test/auth.spec.ts
+++ js/core/test/auth.spec.ts
@@ -93,7 +93,7 @@ test("Bearer authorisation", async () => {
   expect(bearerAuth.sessionData?.userId instanceof Uuidv4).toBeTrue();
 });
 
-test.only("Bearer refresh", async () => {
+test("Bearer refresh", async () => {
   const core = await createAuthenticatedCore("bearer-refresh@air.day");
   const bearerAuth = core.auth as BearerAuth;
   const ogToken = bearerAuth.sessionToken;
