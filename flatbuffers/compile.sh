858c3872900a61222cf5222112bdb4668d020273
Daniel Gormly <danielgormly@hey.com>
2025-12-10 14:55:05 +1100

    Removed previous fb compilation

--- flatbuffers/compile.sh
+++ flatbuffers/compile.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 WORK_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-flatc --ts -o $WORK_DIR/../js/core/src/ $WORK_DIR/proto.fbs
-flatc --rust -o $WORK_DIR/../server/src/sync/ $WORK_DIR/proto.fbs
+JS_DIR="$WORK_DIR/../js/core/src"
+RS_DIR="$WORK_DIR/../server/src/sync"
+rm -rf $JS_DIR/proto
+rm -rf $RS_DIR/proto_generated.rs
+flatc --ts -o $JS_DIR/ $WORK_DIR/proto.fbs
+flatc --rust -o $RS_DIR/ $WORK_DIR/proto.fbs
 echo Compiled Flatbuffers for Typescript and Rust
