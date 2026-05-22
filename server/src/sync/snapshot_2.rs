40cfef5434c5e6d5f400e259f682485beaa00433
Daniel Gormly <danielgormly@hey.com>
2026-05-22 19:10:13 +1000

    Working through snapshot decision

--- server/src/sync/snapshot_2.rs
+++ server/src/sync/snapshot_2.rs
@@ -1,3 +1,7 @@
+use std::{collections::HashMap, sync::Mutex, time::Duration};
+use tokio::time::Instant;
+use uuid::Uuid;
+
 // If we have 10k ops in our db - it's time to ask clients nicely to snapshot
 // Timeout after 5 minutes
 const SNAPSHOT_THRESHOLD_OPS: u64 = 10_000;
@@ -11,43 +15,89 @@ struct ActiveLease {
 }
 
 enum LeaseState {
-    Idle {
-        last_attempt_ended_at: Option<Instant>,
-    },
+    Idle { cooldown_until: Option<Instant> },
     Active(ActiveLease),
 }
 
 struct SnapshotCoordinator2 {
     timeout: Duration,  // Maximum time to allow device to return a snapshot
-    cooldown: Duration, // Time between snapshots
+    cooldown: Duration, // Time between snapshot attempts
     threshold_ops: u64,
     leases: Mutex<HashMap<Uuid, LeaseState>>,
 }
 
 pub enum Decision {
-  Issue { device_id: DeviceId, lease_id: u64, up_to_op_id: u64 },
-  Skip,
+    Issue {
+        device_id: Uuid,
+        lease_id: u64,
+        up_to_op_id: u64,
+    },
+    Skip,
 }
 
 impl SnapshotCoordinator2 {
     pub fn new() -> Self {
         Self {
             timeout: SNAPSHOT_TIMEOUT,
+            cooldown: SNAPSHOT_TIMEOUT,
             threshold_ops: SNAPSHOT_THRESHOLD_OPS,
+            leases: Mutex::new(HashMap::new()),
         }
     }
     pub fn evaluate(
-      &self,
-      account: Uuid,
-      last_snapshot_up_to: u64, // local threshold
-      device_id: Uuid,
-      last_acked_op_id,
-      now: Instant,
+        &self,
+        account: Uuid,
+        server_snapshot_op_id: u64,   // last snapshot on server
+        server_last_op_id: u64,       // latest op on device
+        device_last_acked_op_id: u64, // last acked op on device
+        device_id: Uuid,
+        now: Instant,
     ) -> Decision {
-      let mut leases = self.leases.lock();
-      let state = leases.entry(account).or_insert_with(|| LeaseState::Idle {
-        last_attempt_ended_at: None });
-      Decision::Skip
+        // No need to snapshot yet
+        if server_last_op_id - server_snapshot_op_id < self.threshold_ops {
+            return Decision::Skip;
+        }
+        // Client is not yet up to date with server
+        if server_last_op_id != device_last_acked_op_id {
+            return Decision::Skip;
+        }
+        // Ok we are snapshot viable
+        let mut leases = self
+            .leases
+            .lock()
+            .expect("snapshot coordinator mutex poisoned");
+        let lease_state = leases.entry(account).or_insert_with(|| LeaseState::Idle {
+            cooldown_until: None,
+        });
+        let expired = match lease_state {
+            LeaseState::Active(lease) if now >= lease.expires_at => true, // Expired lease
+            LeaseState::Active(_) => return Decision::Skip,               // In-flight lease
+            LeaseState::Idle { .. } => false, // Could be idle, but not expired lease
+        };
+        if expired {
+            // Prevent attempt for 5 minutes
+            *lease_state = LeaseState::Idle {
+                cooldown_until: Some(now + Duration::from_secs(5 * 60)),
+            };
+            return Decision::Skip;
+        }
+        if let LeaseState::Idle { cooldown_until } = lease_state {
+            let in_cooldown = match cooldown_until {
+                None => false,
+                Some(cooldown) => now < *cooldown,
+            };
+            if in_cooldown {
+                return Decision::Skip;
+            }
+        }
+        // Ok we're ready
+        let lease_id = 0; // TODO: get next id
+                          // Create lease
+        Decision::Issue {
+            device_id,
+            lease_id,
+            up_to_op_id: device_last_acked_op_id,
+        }
     }
 }
 
@@ -58,6 +108,8 @@ mod tests {
     #[test]
     fn ensure_snapshot_attempt_when_threshold_exceeded() {
         let coordinator = SnapshotCoordinator2::new();
-        coordinator.evaluate(Uuid::new(),
+        coordinator.evaluate(Uuid::new())
     }
+
+    // TODO: Race these
 }
